26719b8386e7ecc695e287c60c3ba1bff38f5031
facundoestradaa <es.facundo.08@gmail.com>
2026-04-27 23:21:39 -0300

    Update powers.py

--- powers.py
+++ powers.py
@@ -1,4 +1,6 @@
 # Replace the "ANSWER HERE" for your answer
+from sys import base_exec_prefix
+
 
 def power(base, exp):
     """
@@ -7,7 +9,16 @@ def power(base, exp):
 
     Ejemplo: power(2, 3) -> 8  (2*2*2)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+
+    if exp == 0:
+        return 1
+
+    else:
+        resultado =1
+        for i in range(exp):
+            resultado = resultado*base
+            print(resultado)
+    return resultado
 
 
 def sum_of_powers(base, max_exp):
@@ -17,4 +28,10 @@ def sum_of_powers(base, max_exp):
 
     Ejemplo: sum_of_powers(2, 3) -> 15  (1+2+4+8)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+
+    total = 0
+    for expo in range(0, max_exp+1):
+        valor = power(base, expo)
+        total = total + valor
+        print(total)
+    return total
